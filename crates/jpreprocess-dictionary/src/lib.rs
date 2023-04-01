16f01c066413c82fb8554d61e47981416e7be110
femshima <49227365+femshima@users.noreply.github.com>
2023-04-01 16:36:22 +0900

    cargo fmt

--- crates/jpreprocess-dictionary/src/lib.rs
+++ crates/jpreprocess-dictionary/src/lib.rs
@@ -2,6 +2,6 @@ mod dictionary;
 mod jpreprocess_dictionary;
 mod lindera_dictionary;
 
-pub use dictionary::*;
 pub use crate::jpreprocess_dictionary::JPreprocessDictionary;
 pub use crate::lindera_dictionary::LinderaDictionary;
+pub use dictionary::*;
