2a91a4373ed09b56907a72024a10d72f3f6ce1b3
femshima <49227365+femshima@users.noreply.github.com>
2023-06-01 12:32:13 +0900

    assert Send and Sync is implemented in Utterance

--- crates/jpreprocess-jpcommon/src/label/utterance.rs
+++ crates/jpreprocess-jpcommon/src/label/utterance.rs
@@ -65,3 +65,21 @@ impl From<&[NJDNode]> for Utterance {
         Self { breath_groups }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::Utterance;
+
+    #[test]
+    fn test_send() {
+        fn assert_send<T: Send>() {}
+        assert_send::<Utterance>();
+    }
+
+    #[test]
+    fn test_sync() {
+        fn assert_sync<T: Sync>() {}
+        assert_sync::<Utterance>();
+    }
+}
