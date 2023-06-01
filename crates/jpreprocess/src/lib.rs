7fd0d95ea1a4b6b4056174a958dd97aa99360ec1
femshima <49227365+femshima@users.noreply.github.com>
2023-06-02 06:16:00 +0900

    make example easier to read

--- crates/jpreprocess/src/lib.rs
+++ crates/jpreprocess/src/lib.rs
@@ -12,8 +12,25 @@
 //! let config = JPreprocessDictionaryConfig::FileLindera(path);
 //! let jpreprocess = JPreprocess::new(config)?;
 //!
-//! let jpcommon_label = jpreprocess.extract_fullcontext("日本語文を解析し、音声合成エンジンに渡せる形式に変換します．")?;
-//! assert_eq!(jpcommon_label[2], "sil^n-i+h=o/A:-3+1+7/B:xx-xx_xx/C:02_xx+xx/D:02+xx_xx/E:xx_xx!xx_xx-xx/F:7_4#0_xx@1_3|1_12/G:4_4%0_xx_1/H:xx_xx/I:3-12@1+2&1-8|1+41/J:5_29/K:2+8-41");
+//! let jpcommon_label = jpreprocess
+//!     .extract_fullcontext("日本語文を解析し、音声合成エンジンに渡せる形式に変換します．")?;
+//! assert_eq!(
+//!   jpcommon_label[2],
+//!   concat!(
+//!       "sil^n-i+h=o",
+//!       "/A:-3+1+7",
+//!       "/B:xx-xx_xx",
+//!       "/C:02_xx+xx",
+//!       "/D:02+xx_xx",
+//!       "/E:xx_xx!xx_xx-xx",
+//!       "/F:7_4#0_xx@1_3|1_12",
+//!       "/G:4_4%0_xx_1",
+//!       "/H:xx_xx",
+//!       "/I:3-12@1+2&1-8|1+41",
+//!       "/J:5_29",
+//!       "/K:2+8-41"
+//!   )
+//! );
 //! #
 //! #     Ok(())
 //! # }
