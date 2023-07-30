9084ae4d686e28e6cf22dcda25d8e77b855fdcd3
femshima <49227365+femshima@users.noreply.github.com>
2023-07-30 21:25:00 +0900

    metadata is empty when end==0

--- crates/jpreprocess-dictionary/src/metadata.rs
+++ crates/jpreprocess-dictionary/src/metadata.rs
@@ -4,6 +4,9 @@ use crate::WordDictionaryMode;
 
 pub fn get_metadata(words_idx_data: &[u8], words_data: &[u8]) -> Option<String> {
     let metadata_end = LittleEndian::read_u32(&words_idx_data[0..4]) as usize;
+    if metadata_end == 0 {
+        return None;
+    }
 
     String::from_utf8(words_data[0..metadata_end].to_vec()).ok()
 }
