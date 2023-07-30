5ccdf861fa4cd1e0ac5130bc3ecf075d145b72ce
femshima <49227365+femshima@users.noreply.github.com>
2023-07-30 13:56:22 +0900

    refactor dictionary builder and add user-dict

--- crates/jpreprocess-dictionary-builder/src/lib.rs
+++ crates/jpreprocess-dictionary-builder/src/lib.rs
@@ -1,2 +1,3 @@
+mod build_dict;
 pub mod ipadic_builder;
 pub mod serializer;
--- crates/jpreprocess-dictionary-builder/src/serializer.rs
+++ crates/jpreprocess-dictionary-builder/src/serializer.rs
@@ -3,6 +3,20 @@ use lindera_core::{error::LinderaErrorKind, LinderaResult};
 pub trait DictionarySerializer {
     fn identifier(&self) -> String;
     fn serialize(&self, row: &[String]) -> LinderaResult<Vec<u8>>;
+    fn simple(&self, row: &[String]) -> LinderaResult<Vec<u8>> {
+        let details = vec![
+            row[1].to_string(), // POS
+            "*".to_string(),    // POS subcategory 1
+            "*".to_string(),    // POS subcategory 2
+            "*".to_string(),    // POS subcategory 3
+            "*".to_string(),    // Conjugation type
+            "*".to_string(),    // Conjugation form
+            row[0].to_string(), // Base form
+            row[2].to_string(), // Reading
+            "*".to_string(),    // Pronunciation
+        ];
+        self.serialize(&details)
+    }
 }
 
 pub struct LinderaSerializer;
@@ -12,7 +26,7 @@ impl DictionarySerializer for LinderaSerializer {
     }
     fn serialize(&self, row: &[String]) -> LinderaResult<Vec<u8>> {
         let mut word_detail = Vec::new();
-        for item in row.iter().skip(4) {
+        for item in row.iter() {
             word_detail.push(item.to_string());
         }
         bincode::serialize(&word_detail)
@@ -27,7 +41,7 @@ impl DictionarySerializer for JPreprocessSerializer {
     }
     fn serialize(&self, row: &[String]) -> LinderaResult<Vec<u8>> {
         use jpreprocess_core::word_entry::WordEntry;
-        let mut str_details = row.iter().skip(4).map(|d| &d[..]).collect::<Vec<&str>>();
+        let mut str_details = row.iter().map(|d| &d[..]).collect::<Vec<&str>>();
         str_details.resize(13, "");
         match WordEntry::load(&str_details[..]) {
             Ok(entry) => bincode::serialize(&entry)
