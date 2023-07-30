42fc1d8985bc6cfd4ae8265b71f9869b20825995
femshima <49227365+femshima@users.noreply.github.com>
2023-07-30 13:58:04 +0900

    add user dictionary test

---
+++ crates/jpreprocess/tests/user_dictionary.rs
@@ -0,0 +1,66 @@
+use std::{error::Error, path::PathBuf};
+
+use jpreprocess::{JPreprocess, JPreprocessConfig, SystemDictionaryConfig};
+use jpreprocess_dictionary_builder::{
+    ipadic_builder::IpadicBuilder, serializer::JPreprocessSerializer,
+};
+
+#[test]
+#[ignore]
+fn system_dictionary() -> Result<(), Box<dyn Error>> {
+    #[cfg(feature = "naist-jdic")]
+    let config = SystemDictionaryConfig::Bundled(JPreprocessDictionaryKind::NaistJdic);
+    #[cfg(not(feature = "naist-jdic"))]
+    let config = SystemDictionaryConfig::File(PathBuf::from("tests/dict"));
+
+    let jpreprocess = JPreprocess::from_config(JPreprocessConfig {
+        dictionary: config,
+        user_dictionary: None,
+    })
+    .unwrap();
+    let njd = jpreprocess.text_to_njd("クーバネティス")?;
+
+    assert_eq!(njd.nodes[0].get_string(), "ク");
+    assert_eq!(njd.nodes[1].get_string(), "ーバネティス");
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn lindera_user_dictionary() -> Result<(), Box<dyn Error>> {
+    #[cfg(feature = "naist-jdic")]
+    let config = SystemDictionaryConfig::Bundled(JPreprocessDictionaryKind::NaistJdic);
+    #[cfg(not(feature = "naist-jdic"))]
+    let config = SystemDictionaryConfig::File(PathBuf::from("tests/dict"));
+
+    let mut rows: Vec<Vec<&str>> = vec![vec![
+        "クーバネティス",
+        "1348",
+        "1348",
+        "-28650",
+        "名詞",
+        "固有名詞",
+        "一般",
+        "*",
+        "*",
+        "*",
+        "Kubernetes",
+        "クーバネティス",
+        "クーバネティス",
+        "4/5",
+        "*",
+    ]];
+    rows.sort_by_key(|row| row[0].to_string());
+
+    let dictionary = config.load()?;
+    let user_dictionary =
+        IpadicBuilder::new(Box::new(JPreprocessSerializer)).build_user_dict_from_data(&rows)?;
+
+    let jpreprocess = JPreprocess::new(dictionary, Some(user_dictionary));
+    let njd = jpreprocess.text_to_njd("クーバネティス")?;
+
+    assert_eq!(njd.nodes[0].get_string(), "クーバネティス");
+
+    Ok(())
+}
