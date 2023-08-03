8c11ac4c0e37fb8183dfbf5e5db861ffeb6a2496
femshima <49227365+femshima@users.noreply.github.com>
2023-08-03 16:00:33 +0900

    add user dict to jpreprocess bin

--- crates/jpreprocess/src/bin/jpreprocess.rs
+++ crates/jpreprocess/src/bin/jpreprocess.rs
@@ -4,12 +4,17 @@ use std::path::PathBuf;
 use jpreprocess::*;
 
 use clap::{Args, Parser};
+use lindera_dictionary::UserDictionaryConfig;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(flatten)]
     dict: DictionaryArgs,
+
+    /// The location of the user dictionary
+    #[arg(short, long)]
+    user_dictionary: Option<PathBuf>,
 
     /// The text to be processed
     input: String,
@@ -18,15 +23,12 @@ struct Cli {
 #[derive(Args, Debug)]
 #[group(required = true, multiple = false)]
 struct DictionaryArgs {
-    /// The location of lindera dictionary
+    /// The location of the system dictionary
     #[arg(short, long)]
-    lindera_dictionary: Option<PathBuf>,
-
-    /// The location of jpreprocess dictionary
-    #[arg(short, long)]
-    jpreprocess_dictionary: Option<PathBuf>,
+    dictionary: Option<PathBuf>,
 
     /// Use bundled naist-jdic dictionary
+    #[cfg(feature = "naist-jdic")]
     #[arg(short, long)]
     naist_jdic: bool,
 }
@@ -34,38 +36,27 @@ struct DictionaryArgs {
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
-    let dictionary = if let Some(dict) = cli.dict.jpreprocess_dictionary {
-        SystemDictionaryConfig::File(dict)
-    } else if let Some(dict) = cli.dict.lindera_dictionary {
+    let dictionary = if let Some(dict) = cli.dict.dictionary {
         SystemDictionaryConfig::File(dict)
     } else {
-        let args_error = || {
-            use clap::{error::ErrorKind, CommandFactory};
-            let mut cmd = Cli::command();
-            cmd.error(
-                ErrorKind::ValueValidation,
-                concat!(
-                    "This build of jpreprocess does not have the bundled dictionary. ",
-                    "Please use other dictionary type."
-                ),
-            )
-            .exit()
-        };
-        if cli.dict.naist_jdic {
-            #[cfg(not(feature = "naist-jdic"))]
-            {
-                args_error()
-            }
-            #[cfg(feature = "naist-jdic")]
-            SystemDictionaryConfig::Bundled(kind::JPreprocessDictionaryKind::NaistJdic)
-        } else {
-            args_error()
-        }
+        #[cfg(not(feature = "naist-jdic"))]
+        unreachable!("This build of jpreprocess does not have the bundled dictionary, and it is not supporsed to reach here.");
+        #[cfg(feature = "naist-jdic")]
+        SystemDictionaryConfig::Bundled(kind::JPreprocessDictionaryKind::NaistJdic)
+    };
+
+    let user_dictionary = if let Some(user_dict) = cli.user_dictionary {
+        Some(UserDictionaryConfig {
+            path: user_dict,
+            kind: Some(lindera_dictionary::DictionaryKind::IPADIC),
+        })
+    } else {
+        None
     };
 
     let jpreprocess = JPreprocess::from_config(JPreprocessConfig {
         dictionary,
-        user_dictionary: None,
+        user_dictionary,
     })?;
 
     let njd = jpreprocess.run_frontend(&cli.input)?;
