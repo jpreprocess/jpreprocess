0675f7a216cecfbcf7f3e8251f3a5a099fb52b89
femshima <49227365+femshima@users.noreply.github.com>
2024-02-06 12:48:41 +0900

    fix text is not cleared

--- examples/example-simple/src/main.rs
+++ examples/example-simple/src/main.rs
@@ -18,7 +18,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut text = String::new();
     while std::io::stdin().read_line(&mut text).is_ok() {
         let jpcommon_label = jpreprocess.extract_fullcontext(&text)?;
-        println!("{}", jpcommon_label.join("\n"))
+        println!("{}", jpcommon_label.join("\n"));
+        text.clear();
     }
 
     Ok(())
