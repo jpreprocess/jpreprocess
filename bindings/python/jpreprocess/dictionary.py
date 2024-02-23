4b1ed74557f843ddecc5d214541ebc3fcc7b2bf7
femshima <49227365+femshima@users.noreply.github.com>
2024-02-23 15:56:13 +0900

    moderinize dictionary.py

--- bindings/python/jpreprocess/dictionary.py
+++ bindings/python/jpreprocess/dictionary.py
@@ -1,57 +1,48 @@
 from .jpreprocess import JPREPROCESS_VERSION
-
+from contextlib import ExitStack
+import sys
 import os
+import atexit
+
+# from https://github.com/r9y9/pyopenjtalk/pull/74
+
+if sys.version_info >= (3, 9):
+    from importlib.resources import as_file, files
+else:
+    from importlib_resources import as_file, files
+
+_file_manager = ExitStack()
+atexit.register(_file_manager.close)
+_file_ref = files(__package__)
 
 # Dictionary path
 # defaults to the package directory where the dictionary will be automatically downloaded
 JPREPROCESS_DICT_PATH = os.environ.get(
     "JPREPROCESS_DICT_PATH",
-    pkg_resources.resource_filename(
-        __name__, f"{JPREPROCESS_VERSION}/naist-jdic"),
+    str(_file_manager.enter_context(
+        as_file(_file_ref / JPREPROCESS_VERSION / "naist-jdic"))),
 )
 JPREPROCESS_DICT_URL = f"https://github.com/jpreprocess/jpreprocess/releases/download/v{JPREPROCESS_VERSION}/naist-jdic-jpreprocess.tar.gz"
 
 
-def download_dictionary():
-    global JPREPROCESS_DICT_PATH
-
-    from urllib import request as urllib
+def download_dictionary() -> None:
+    from urllib.request import urlopen
     import tarfile
+    import tempfile
 
-    filename = pkg_resources.resource_filename(
-        __name__, "naist-jdic-jpreprocess.tar.gz")
-    print(f'Downloading: "{JPREPROCESS_DICT_URL}"')
-
-    try:
-        from tqdm.auto import tqdm
-
-        class TqdmUpTo(tqdm):
-            def __init__(self):
-                super(TqdmUpTo, self).__init__(
-                    unit="B",
-                    unit_scale=True,
-                    unit_divisor=1024,
-                    miniters=1,
-                    desc="naist-jdic-jpreprocess.tar.gz",
-                )
-
-            def update_to(self, b=1, bsize=1, tsize=None):
-                if tsize is not None:
-                    self.total = tsize
-                return self.update(b * bsize - self.n)
-
-        with TqdmUpTo() as t:
-            urllib.urlretrieve(JPREPROCESS_DICT_URL, filename,
-                               reporthook=t.update_to)
-            t.total = t.n
-
-    except ImportError:
-        urllib.urlretrieve(JPREPROCESS_DICT_URL, filename)
-
-    print(f"Extracting tar file {filename}")
-    extract_dir = pkg_resources.resource_filename(
-        __name__, JPREPROCESS_VERSION)
-    os.mkdir(extract_dir)
-    with tarfile.open(filename, mode="r|gz") as f:
-        f.extractall(path=extract_dir)
-    os.remove(filename)
+    with tempfile.TemporaryFile() as file:
+        print('Downloading: "{}"'.format(JPREPROCESS_DICT_URL))
+        with urlopen(JPREPROCESS_DICT_URL) as response:
+            try:
+                from tqdm.auto import tqdm
+                with tqdm.wrapattr(file, "write", total=getattr(response, "length", None)) as tar:
+                    for chunk in response:
+                        tar.write(chunk)
+            except ImportError:
+                for chunk in response:
+                    file.write(chunk)
+        file.seek(0)
+        print("Extracting tar file")
+        with tarfile.open(mode="r|gz", fileobj=file) as f, as_file(_file_ref / JPREPROCESS_VERSION) as dir:
+            f.extractall(path=dir)
+        print("done")
