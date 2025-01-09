c153c9b7bacbbbe1d4111828510595ee204c3250
femshima <49227365+femshima@users.noreply.github.com>
2025-01-10 07:06:35 +0900

    impl WordDictionary::from_linderadict

--- crates/jpreprocess-dictionary/src/tokenizer/default.rs
+++ crates/jpreprocess-dictionary/src/tokenizer/default.rs
@@ -10,6 +10,39 @@ pub struct WordDictionary {
 }
 
 impl WordDictionary {
+    fn from_linderadict(
+        system: &lindera_dictionary::dictionary::Dictionary,
+        user: Option<&lindera_dictionary::dictionary::UserDictionary>,
+    ) -> Self {
+        fn parse_words(words_idx_data: &[u8], words_data: &[u8]) -> Vec<WordEntry> {
+            let idx_vec = words_idx_data
+                .chunks(4)
+                .map(|chunk| {
+                    assert!(chunk.len() == 4);
+                    u32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]])
+                })
+                .collect::<Vec<_>>();
+            let mut parsed = Vec::with_capacity(idx_vec.len());
+            for i in 0..idx_vec.len() {
+                let start = idx_vec[i] as usize;
+                let end = if i + 1 < idx_vec.len() {
+                    idx_vec[i + 1] as usize
+                } else {
+                    words_data.len()
+                };
+                let entry = bincode::deserialize(&words_data[start..end]).unwrap();
+                parsed.push(entry);
+            }
+            parsed
+        }
+        Self {
+            system: parse_words(
+                &system.prefix_dictionary.words_idx_data,
+                &system.prefix_dictionary.words_data,
+            ),
+            user: user.map(|user| parse_words(&user.dict.words_idx_data, &user.dict.words_data)),
+        }
+    }
     fn get(&self, word_id: lindera::dictionary::WordId) -> WordEntry {
         let word = if word_id.is_unknown() {
             None
@@ -30,8 +63,12 @@ pub struct DefaultTokenizer {
 }
 
 impl DefaultTokenizer {
-    pub fn new(config: lindera::tokenizer::TokenizerConfig, word_dictionary: WordDictionary) -> Self {
-        let tokenizer = lindera::tokenizer::Tokenizer::from_config(&config).unwrap();
+    pub fn new(segmenter: lindera::segmenter::Segmenter) -> Self {
+        let word_dictionary = WordDictionary::from_linderadict(
+            &segmenter.dictionary,
+            segmenter.user_dictionary.as_ref(),
+        );
+        let tokenizer = lindera::tokenizer::Tokenizer::new(segmenter);
         Self {
             tokenizer,
             word_dictionary,
