c49fd45e4417a39e94e2e347ae9770481bfd4c88
femshima <49227365+femshima@users.noreply.github.com>
2025-01-11 07:20:05 +0900

    improve efficiency of DefaultTokenizer

--- crates/jpreprocess-dictionary/src/tokenizer/default.rs
+++ crates/jpreprocess-dictionary/src/tokenizer/default.rs
@@ -1,77 +1,68 @@
 use std::borrow::Cow;
 
-use jpreprocess_core::word_entry::WordEntry;
+use jpreprocess_core::{error::DictionaryError, word_entry::WordEntry};
 
 use super::{Token, Tokenizer};
 
-pub struct WordDictionary {
-    system: Vec<WordEntry>,
-    user: Option<Vec<WordEntry>>,
-}
-
-impl WordDictionary {
-    fn from_linderadict(
-        system: &lindera_dictionary::dictionary::Dictionary,
-        user: Option<&lindera_dictionary::dictionary::UserDictionary>,
-    ) -> Self {
-        fn parse_words(words_idx_data: &[u8], words_data: &[u8]) -> Vec<WordEntry> {
-            let idx_vec = words_idx_data
-                .chunks(4)
-                .map(|chunk| {
-                    assert!(chunk.len() == 4);
-                    u32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]])
-                })
-                .collect::<Vec<_>>();
-            let mut parsed = Vec::with_capacity(idx_vec.len());
-            for i in 0..idx_vec.len() {
-                let start = idx_vec[i] as usize;
-                let end = if i + 1 < idx_vec.len() {
-                    idx_vec[i + 1] as usize
-                } else {
-                    words_data.len()
-                };
-                let entry = bincode::deserialize(&words_data[start..end]).unwrap();
-                parsed.push(entry);
-            }
-            parsed
-        }
-        Self {
-            system: parse_words(
-                &system.prefix_dictionary.words_idx_data,
-                &system.prefix_dictionary.words_data,
-            ),
-            user: user.map(|user| parse_words(&user.dict.words_idx_data, &user.dict.words_data)),
-        }
-    }
-    fn get(&self, word_id: lindera::dictionary::WordId) -> WordEntry {
-        let word = if word_id.is_unknown() {
-            None
-        } else if word_id.is_system() {
-            self.system.get(word_id.id as usize)
-        } else {
-            self.user
-                .as_ref()
-                .and_then(|user| user.get(word_id.id as usize))
-        };
-        word.cloned().unwrap_or_default()
+fn get_word_data<'a>(idx: &[u8], data: &'a [u8], word_id: usize) -> &'a [u8] {
+    if word_id * 4 + 4 > idx.len() {
+        &[]
+    } else if word_id * 4 + 8 > idx.len() {
+        let start = u32::from_le_bytes([
+            idx[word_id * 4],
+            idx[word_id * 4 + 1],
+            idx[word_id * 4 + 2],
+            idx[word_id * 4 + 3],
+        ]) as usize;
+        &data[start..]
+    } else {
+        let start = u32::from_le_bytes([
+            idx[word_id * 4],
+            idx[word_id * 4 + 1],
+            idx[word_id * 4 + 2],
+            idx[word_id * 4 + 3],
+        ]) as usize;
+        let end = u32::from_le_bytes([
+            idx[word_id * 4 + 4],
+            idx[word_id * 4 + 5],
+            idx[word_id * 4 + 6],
+            idx[word_id * 4 + 7],
+        ]) as usize;
+        &data[start..end]
     }
 }
 
 pub struct DefaultTokenizer {
     tokenizer: lindera::tokenizer::Tokenizer,
-    word_dictionary: WordDictionary,
 }
 
 impl DefaultTokenizer {
-    pub fn new(segmenter: lindera::segmenter::Segmenter) -> Self {
-        let word_dictionary = WordDictionary::from_linderadict(
-            &segmenter.dictionary,
-            segmenter.user_dictionary.as_ref(),
-        );
-        let tokenizer = lindera::tokenizer::Tokenizer::new(segmenter);
-        Self {
-            tokenizer,
-            word_dictionary,
+    pub fn new(tokenizer: lindera::tokenizer::Tokenizer) -> Self {
+        Self { tokenizer }
+    }
+
+    fn get_word(&self, word_id: lindera::dictionary::WordId) -> Result<WordEntry, DictionaryError> {
+        if word_id.is_unknown() {
+            Ok(WordEntry::default())
+        } else if word_id.is_system() {
+            let system = &self.tokenizer.segmenter.dictionary.prefix_dictionary;
+            let data = get_word_data(
+                &system.words_idx_data,
+                &system.words_data,
+                word_id.id as usize,
+            );
+            Ok(bincode::deserialize(data)?)
+        } else {
+            let user = &self.tokenizer.segmenter.user_dictionary;
+            user.as_ref()
+                .map_or(Err(DictionaryError::UserDictionaryNotProvided), |user| {
+                    let data = get_word_data(
+                        &user.dict.words_idx_data,
+                        &user.dict.words_data,
+                        word_id.id as usize,
+                    );
+                    Ok(bincode::deserialize(data)?)
+                })
         }
     }
 }
@@ -83,7 +74,7 @@ impl Tokenizer for DefaultTokenizer {
             .into_iter()
             .map(|token| JPreprocessToken {
                 text: token.text,
-                entry: self.word_dictionary.get(token.word_id),
+                entry: self.get_word(token.word_id).unwrap(),
             })
             .collect()
     }
