e98da95b2e9ae27919627317cf60d059e99fc943
femshima <49227365+femshima@users.noreply.github.com>
2025-02-02 13:28:11 +0900

    fix dict ident

--- crates/jpreprocess-dictionary/src/tokenizer/identify_dictionary.rs
+++ crates/jpreprocess-dictionary/src/tokenizer/identify_dictionary.rs
@@ -13,7 +13,9 @@ impl DictionaryIdent {
 
         match std::str::from_utf8(data) {
             Ok("") => DictionaryIdent::Lindera,
-            Ok(ident) if ident.starts_with("jpreprocess") => DictionaryIdent::JPreprocess,
+            Ok(ident) if ident.to_lowercase().starts_with("jpreprocess") => {
+                DictionaryIdent::JPreprocess
+            }
             Err(e) => {
                 log::warn!("Error parsing dictionary type: {}", e);
                 DictionaryIdent::Lindera
