08bd4b29d21f120d64282d50f9e685c87404106e
femshima <49227365+femshima@users.noreply.github.com>
2025-02-11 16:34:08 +0900

    add 「少し金」 as a unit test

--- crates/jpreprocess-njd/src/open_jtalk/unvoiced_vowel.rs
+++ crates/jpreprocess-njd/src/open_jtalk/unvoiced_vowel.rs
@@ -203,3 +203,31 @@ fn apply_unvoice_rule(mora_curr: &Mora, mora_next: Option<&Mora>) -> Option<bool
         _ => true,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use jpreprocess_core::pronunciation::{Mora, MoraEnum};
+
+    use crate::{unvoiced_vowel::njd_set_unvoiced_vowel, NJD};
+
+    #[test]
+    fn gold() {
+        let mut njd: NJD = [
+            "少,接頭詞,名詞接続,*,*,*,*,少,ショウ,ショー,3/2,P2,-1",
+            "し金,名詞,一般,*,*,*,*,し金,シキン,シキン,1/3,C1,1",
+        ]
+        .into_iter()
+        .collect();
+
+        njd_set_unvoiced_vowel(&mut njd);
+
+        let pron = njd.nodes[1].get_pron();
+        assert_eq!(
+            pron.moras()[0],
+            Mora {
+                mora_enum: MoraEnum::Shi,
+                is_voiced: true
+            }
+        );
+    }
+}
