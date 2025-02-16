925ffc6e10f4893252649744540d94fbb6a43552
phenylshima <49227365+phenylshima@users.noreply.github.com>
2025-02-16 19:32:31 +0900

    Check if shi mora is independent (#425)
    * check if shi mora is independent
    
    * add test
    
    ---------
    
    Co-authored-by: femshima <49227365+femshima@users.noreply.github.com>

--- crates/jpreprocess-njd/src/open_jtalk/unvoiced_vowel.rs
+++ crates/jpreprocess-njd/src/open_jtalk/unvoiced_vowel.rs
@@ -102,7 +102,11 @@ pub fn njd_set_unvoiced_vowel(njd: &mut NJD) {
                 state_next.pos,
                 POS::Doushi(_) | POS::Jodoushi | POS::Joshi(_)
             );
-            let mora_ok = matches!(state_next.mora.mora_enum, MoraEnum::Shi);
+            let mora_ok = matches!(state_next.mora.mora_enum, MoraEnum::Shi)
+                && state_curr.node_index != state_next.node_index
+                && state_nextnext
+                    .as_ref()
+                    .map_or(true, |nn| nn.node_index != state_next.node_index);
             if is_voiced_ok && pos_ok && mora_ok {
                 state_next.is_voiced_flag = if state_next.atype == state_next.midx + 1 {
                     /* rule 4 */
@@ -217,6 +221,47 @@ mod tests {
         let mut njd: NJD = [
             "少,接頭詞,名詞接続,*,*,*,*,少,ショウ,ショー,3/2,P2,-1",
             "し金,名詞,一般,*,*,*,*,し金,シキン,シキン,1/3,C1,1",
+        ]
+        .into_iter()
+        .collect();
+
+        njd_set_unvoiced_vowel(&mut njd);
+
+        let pron = njd.nodes[1].get_pron();
+        assert_eq!(
+            pron.moras()[0],
+            Mora {
+                mora_enum: MoraEnum::Shi,
+                is_voiced: true
+            }
+        );
+    }
+
+    #[test]
+    fn interpretation() {
+        let mut njd: NJD = [
+            "解釈,名詞,サ変接続,*,*,*,*,解釈,カイシャク,カイシャク,1/4,C1,-1",
+            "し,動詞,自立,*,*,サ変・スル,連用形,し,シ,シ,0/1,*,-1",
+            "て,助詞,接続助詞,*,*,*,*,て,テ,テ,0/1,動詞%F1/形容詞%F1/名詞%F5,-1",
+        ]
+        .into_iter()
+        .collect();
+
+        njd_set_unvoiced_vowel(&mut njd);
+
+        let pron = njd.nodes[1].get_pron();
+        assert_eq!(
+            pron.moras()[0],
+            Mora {
+                mora_enum: MoraEnum::Shi,
+                is_voiced: false
+            }
+        );
+
+        let mut njd: NJD = [
+            "解釈,名詞,サ変接続,*,*,*,*,解釈,カイシャク,カイシャク,1/4,C1,-1",
+            "してやれ,動詞,自立,*,*,五段・ラ行,仮定形,してやれ,シテヤレ,シテヤレ,3/4,*,-1",
+            "ば,助詞,接続助詞,*,*,*,*,ば,バ,バ,0/1,動詞%F2/形容詞%F1,-1",
         ]
         .into_iter()
         .collect();
