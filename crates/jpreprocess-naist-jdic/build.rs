5bf7d58351081a757189ac1e03ff1154d5171c4a
femshima <49227365+femshima@users.noreply.github.com>
2025-05-06 17:09:56 +0900

    fix naist-jdic

--- crates/jpreprocess-naist-jdic/build.rs
+++ crates/jpreprocess-naist-jdic/build.rs
@@ -8,10 +8,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
             file_name: "v0.1.3.tar.gz",
             input_dir: "naist-jdic-0.1.3",
             output_dir: "naist-jdic",
-            download_url:
+            download_urls: &[
                 "https://github.com/jpreprocess/naist-jdic/archive/refs/tags/v0.1.3.tar.gz",
+            ],
             dummy_input:
                 "テスト,1343,1343,3195,名詞,サ変接続,*,*,*,*,テスト,テスト,テスト,1/3,C1\n",
+            md5_hash: "a27d2548ecc8e76242c056e5644a2e57",
         },
         jpreprocess_dictionary::dictionary::to_dict::JPreprocessDictionaryBuilder::new(),
     )
